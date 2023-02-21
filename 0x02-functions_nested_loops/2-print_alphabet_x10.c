9846c07a85de547e353ba430209b2ef5463d3bb8
macharia-14 <machariaelick1454@gmail.com>
2023-02-21 03:10:43 -0800

    printing the alphabets 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a function that prints the alphabets 10 times
+ *
+ * Return= Always 0
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int c, n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			putchar(c);
+		}
+		n++;
+
+		putchar('\n')
+	}
+}
