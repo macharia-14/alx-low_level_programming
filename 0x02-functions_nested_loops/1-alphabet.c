3b0516638f298bb2ac7aee8e67fc7c693ae77640
macharia-14 <machariaelick1454@gmail.com>
2023-02-21 02:34:48 -0800

    print the alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - print the alphabets
+ *
+ * Return: Always 0
+ *
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+}
