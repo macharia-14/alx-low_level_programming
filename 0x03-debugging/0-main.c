0d586240eb0590abf5e8f60c9cc48a0ff950aacb
macharia-14 <machariaelick1454@gmail.com>
2023-02-23 03:40:49 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,14 @@
 #include "main.h"
 
 /**
- *
  * main - tests function that prints if integer is potitive or negative
- *
- * Return: Always 0
- *
+ * Return: 0
  */
 int main(void)
 {
 	int i;
 	i=0;
 	positive or negative(i);
-
 	return(0);
 }
 	
